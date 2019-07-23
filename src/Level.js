85db966da621fa0b7f9e10599554ad9dd2c377ef
Dipendra Shrestha <dipendrashrestha059@gmail.com>
2019-07-24 01:23:09 +0545

    level map refactored

--- src/Level.js
+++ src/Level.js
@@ -1,76 +1,7 @@
 class Level {
-  static initClass() {
-    this.maps = [
-      {
-        player: {
-          x: 2,
-          y: 8
-        },
-        tiles: [
-          'BBBBBBBBBBBBBBBBBBBG',
-          'BP               RBG',
-          'B  D   D   T   D  BG',
-          'B  B   B   B   B  BG',
-          'BD   D   D   D   DBG',
-          'BB   B   B   B   BBG',
-          'BD     D          BG',
-          'B   BBBB   BBBBBB BG',
-          'B+         B=     BG',
-          'BBBBBBBBBBBBBBBBBBBG'
-        ]
-      },
-      {
-        player: {
-          x: 1,
-          y: 8
-        },
-        tiles: [
-          'BBBBBBBBBBBBBBBBBBBBBBBBBBBB  BB  BBBBBBBBBBBBBBBB      ',
-          'BR     D            P         BB               =BB      ',
-          'B                       BBBBB BB BBBBBBBBBBBBBBBBB      ',
-          'B-  -        -         BB     BB     B                  ',
-          'B       ---   B       BB  BBBBBBBBB  B                  ',
-          'B --     B   TB ----- B  BB PB    B  B                  ',
-          'B        B -  B       B B    B  B B BB                  ',
-          'B   --- RB    B DDDDD B B BB B BB    B                  ',
-          'B        BD  -B       B   BP   PB  BPB                  ',
-          'BBBFFFFFFBFFFFBWWWWWWWBBBBBBBBBBBBBBBBFFFFFFFFFFFFFFFFFF        G'
-        ]
-      },
-      {
-        player: {
-          x: 2,
-          y: 5
-        },
-        tiles: [
-          'GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG Y   GGGG',
-          'GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGE    =GG  Y  GGG',
-          'GQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ         GG   Y  Y',
-          'GD   D    D   Z    D    D      D    D    D      D    D              GGGGGGGGG',
-          'G                                                           G  G    C       W',
-          'G+   S   S   SS    S   S   S   SS   S   S  S    SS                  Y       W',
-          'GQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQFF      FFGG     W',
-          'GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGFF    FFGG      W',
-          'GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGFFJTFFGG       W',
-          'GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGFFFFFFFFF'
-        ],
-        enemies: [
-          {
-            x: 37,
-            y: 3.5
-          },
-          {
-            x: 55,
-            y: 3.5
-          }
-        ]
-      }
-    ];
-  }
-
   constructor(game, n) {
     this.game = game;
-    this.map = Level.maps[n];
+    this.map = new LevelMap(n);
     this.tiles = [];
     this.entities = [];
     this.player = new Player(this.game, this.map.player);
@@ -147,4 +78,3 @@ class Level {
     this.game.canvas.drawTile(tile, i, j);
   }
 }
-Level.initClass();
---
+++ src/Map.js
@@ -0,0 +1,72 @@
+class LevelMap {
+  constructor(level) {
+    this.maps = [
+      {
+        player: {
+          x: 2,
+          y: 8
+        },
+        tiles: [
+          'BBBBBBBBBBBBBBBBBBBG',
+          'BP               RBG',
+          'B  D   D   T   D  BG',
+          'B  B   B   B   B  BG',
+          'BD   D   D   D   DBG',
+          'BB   B   B   B   BBG',
+          'BD     D          BG',
+          'B   BBBB   BBBBBB BG',
+          'B+         B=     BG',
+          'BBBBBBBBBBBBBBBBBBBG'
+        ]
+      },
+      {
+        player: {
+          x: 1,
+          y: 8
+        },
+        tiles: [
+          'BBBBBBBBBBBBBBBBBBBBBBBBBBBB  BB  BBBBBBBBBBBBBBBB      ',
+          'BR     D            P         BB               =BB      ',
+          'B                       BBBBB BB BBBBBBBBBBBBBBBBB      ',
+          'B-  -        -         BB     BB     B                  ',
+          'B       ---   B       BB  BBBBBBBBB  B                  ',
+          'B --     B   TB ----- B  BB PB    B  B                  ',
+          'B        B -  B       B B    B  B B BB                  ',
+          'B   --- RB    B DDDDD B B BB B BB    B                  ',
+          'B        BD  -B       B   BP   PB  BPB                  ',
+          'BBBFFFFFFBFFFFBWWWWWWWBBBBBBBBBBBBBBBBFFFFFFFFFFFFFFFFFF        G'
+        ]
+      },
+      {
+        player: {
+          x: 2,
+          y: 5
+        },
+        tiles: [
+          'GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG Y   GGGG',
+          'GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGE    =GG  Y  GGG',
+          'GQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ         GG   Y  Y',
+          'GD   D    D   Z    D    D      D    D    D      D    D              GGGGGGGGG',
+          'G                                                           G  G    C       W',
+          'G+   S   S   SS    S   S   S   SS   S   S  S    SS                  Y       W',
+          'GQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQFF      FFGG     W',
+          'GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGFF    FFGG      W',
+          'GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGFFJTFFGG       W',
+          'GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGFFFFFFFFF'
+        ],
+        enemies: [
+          {
+            x: 37,
+            y: 3.5
+          },
+          {
+            x: 55,
+            y: 3.5
+          }
+        ]
+      }
+    ];
+
+    return this.maps[level];
+  }
+} 
